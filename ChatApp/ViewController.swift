99c53070b6d595ef5e6a65eb0fc8d98c9b7dcab6
mokemoko <mokemoko331@gmail.com>
2016-05-28 10:55:48 +0900

    test

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import SocketIOClientSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let socket = SocketIOClient(socketURL: NSURL(string: "http://52.68.248.110:8000")!, options: [.Log(true), .ForcePolling(true)])
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        socket.on("list") { data, hoge in
+            print("list")
+        }
+        
+        socket.on("currentAmount") {data, ack in
+            if let cur = data[0] as? Double {
+                socket.emitWithAck("canUpdate", cur)(timeoutAfter: 0) {data in
+                    socket.emit("update", ["amount": cur + 2.50])
+                }
+                
+                ack.with("Got your currentAmount", "dude")
+            }
+        }
+        
+        socket.connect()
     }
 
     override func didReceiveMemoryWarning() {

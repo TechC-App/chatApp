caa76a307ec186ad2b2b1af598d5fc0e78385cd1
mokemoko <mokemoko331@gmail.com>
2016-06-03 18:59:04 +0900

    replace websocket library

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import SwiftWebSocket
 
 class ViewController: UIViewController {
-
+    
+    var messageNum = 0
+    let ws = WebSocket()
+    
     @IBAction func firstViewReturnActionForSegue(segue: UIStoryboardSegue) {
         // anything
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let send: () -> Void = {
+            self.messageNum += 1
+            let msg = "\(self.messageNum): \(NSDate().description)"
+            print("send: \(msg)")
+            self.ws.send(msg)
+        }
+        
+        ws.event.open = {
+            print("opened")
+            send()
+        }
+        ws.event.close = { code, reason, clean in
+            print("close")
+        }
+        ws.event.error = { error in
+            print("error \(error)")
+        }
+        ws.event.message = { message in
+            if let text = message as? String {
+                print("recv: \(text)")
+                if self.messageNum == 10 {
+                    self.ws.close()
+                } else {
+                    send()
+                }
+            }
+        }
+        
+        ws.open("ws://localhost:8080")
     }
 }
--- Podfile
+++ Podfile
@@ -2,6 +2,6 @@ source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 use_frameworks!
 
-target "ChatApp" do
-	pod 'Socket.IO-Client-Swift', '~> 6.1.2' # Or latest version
+target 'ChatApp' do
+	pod 'SwiftWebSocket'
 end
